8c660b5942250ebe1a06da38a5d55d5f5c50c05c
GaoXiong <xiong.gao@shopee.com>
2023-02-09 23:13:15 +0800

    build

--- build.sh
+++ build.sh
@@ -3,13 +3,19 @@ rm -rf dist
 npm run build
 cd ../
 
+cd web
+rm -rf dist
+npm run build
+cd ../
+
 cd server
 rm -rf dist
 rm -rf public
 npm run build
 cd ../
 
-cp -R ui/dist/ server/public
+cp -R ui/dist/ server/public/mobile
+cp -R ui/dist/ server/public/web
 
 git add .
 git commit -m "build"
