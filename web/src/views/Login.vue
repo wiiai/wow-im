330411c914db8985ebbb43c153be087066df5e3b
GaoXiong <xiong.gao@shopee.com>
2023-02-10 00:21:10 +0800

    build

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,33 +1,46 @@
 <template>
-  <van-form class="form" @submit="onSubmit">
-    <div class="title">
-      <h2>欢迎登录</h2>
-    </div>
-
-    <van-cell-group inset>
-      <van-field
+  <div class="container">
+    <v-card class="v-card mx-auto px-6 py-8" max-width="400">
+      <div class="title2">
+        <h2>登录友友IM</h2>
+      </div>
+      <v-text-field
         v-model="account"
-        name="账户"
-        placeholder="账户"
-        :rules="[{ required: true, message: '请填写账户' }]"
-      />
-      <van-field
+        :readonly="loading"
+        class="mb-2"
+        clearable
+        persistent-hint
+        color="primary"
+        label="账户"
+        variant="solo"
+      ></v-text-field>
+
+      <v-text-field
         v-model="password"
-        type="password"
-        name="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
-      />
-    </van-cell-group>
+        :readonly="loading"
+        class="mb-2"
+        clearable
+        persistent-hint
+        color="primary"
+        label="密码"
+        variant="solo"
+      ></v-text-field>
 
-    <div style="margin: 30px">
-      <van-button round block type="primary" native-type="submit">
-        提交
-      </van-button>
-    </div>
-
-    <div class="tip">可使用账号：000001、000002、0000003 密码均为 666666</div>
-  </van-form>
+      <v-btn
+        :onclick="onSubmit"
+        :disabled="!password || !account"
+        :loading="loading"
+        block
+        color="success"
+        size="large"
+        type="submit"
+        variant="elevated"
+      >
+        立即登录
+      </v-btn>
+      <div class="tip">可使用账号：000001、000002、0000003 密码均为 666666</div>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -36,31 +49,42 @@ import { useUserStore } from "@/stores/user";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
-const account = ref("000001");
-const password = ref("666666");
+const account = ref("");
+const password = ref("");
+const loading = ref(false);
 const userStore = useUserStore();
 const router = useRouter();
 const socket = useSocketStore();
 
 const onSubmit = (values: any) => {
+  if (!password.value || !account.value) {
+    return;
+  }
+
+  loading.value = true;
   userStore
     .login({
       account: account.value,
       password: password.value,
     })
     .then(() => {
+      loading.value = false;
       socket.connect();
       socket.initData();
       router.push("/");
-    })
+    });
 };
 </script>
 
 <style scoped>
 .form {
   width: 600px;
-  margin:  0 auto;
+  margin: 0 auto;
   margin-top: 10%;
+}
+
+.v-card {
+  margin-top: -5%;
 }
 
 .title {
@@ -69,8 +93,24 @@ const onSubmit = (values: any) => {
   margin-bottom: 30px;
 }
 
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+  background-color: #f3f3f3;
+}
+
 .tip {
-  padding: 0 30px;
+  margin-top: 16px;
   color: #666;
+}
+
+.title2 {
+  font-size: 24px;
+  margin-bottom: 30px;
+  text-align: center;
 }
 </style>
