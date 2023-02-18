09af79ed0b2928d47bc3f4eb7b5bce222e4acdeb
GaoXiong <xiong.gao@shopee.com>
2023-02-18 21:34:27 +0800

    build

--- server/dist/socket.js
+++ server/dist/socket.js
@@ -8,6 +8,17 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initSocket = void 0;
 const socket_io_1 = require("socket.io");
@@ -162,6 +173,23 @@ const initSocket = (server) => {
                 });
             }
         });
+        // 协议转发
+        function transfer(name) {
+            socket.on(name, data => {
+                const { to } = data, rest = __rest(data, ["to"]);
+                if (userMap[to]) {
+                    userMap[to].emit(name, {
+                        sender: user.id,
+                        nickname: user.nickname,
+                        avatar: user.avatar,
+                        data: rest
+                    });
+                }
+            });
+        }
+        transfer('async');
+        transfer('enter-screen');
+        transfer('enter-screen-answer');
         // 视频聊天
         socket.on('video-call', data => {
             if (userMap[data.to]) {
